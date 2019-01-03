db4944e37e355581d91aadc1170f57358c857f44
Ari <Ari.Darmawannn@gmail.com>
2019-01-03 13:16:39 +0700

    tes pusher

---
+++ resources/js/components/message.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <li class="list-group-item" :class="className"><slot></slot></li>
+        <small class="badge float-right">You</small>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props:[
+          'color'
+        ],
+
+        computed:{
+            className(){
+                return 'list-group-item-' + this.color;
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
